8f3a29c568315f01c88cef3d75ecfed369b2c37d
Alex Mafuru <alex@iaestz.com>
2026-04-01 00:01:58 +0300

    Fix frappe.ready error for v15

--- iaes_custom/public/js/id_before_after.js
+++ iaes_custom/public/js/id_before_after.js
@@ -1,73 +1,61 @@
-(function () {
-	function add_id_filter_buttons(listview) {
-		if (!listview || !listview.page) return;
-		if (listview.page.__id_before_after_added) return;
-		listview.page.__id_before_after_added = true;
+frappe.listview_settings["Purchase Invoice"] = {
+    onload(listview) {
 
-		listview.page.add_inner_button(__("ID Before"), () => {
-			const d = new frappe.ui.Dialog({
-				title: __("Filter by ID Before"),
-				fields: [
-					{
-						label: __("Document ID"),
-						fieldname: "doc_id",
-						fieldtype: "Data",
-						reqd: 1,
-					},
-				],
-				primary_action_label: __("Apply"),
-				primary_action(values) {
-					listview.filter_area.add([
-						[listview.doctype, "name", "<", values.doc_id],
-					]);
-					d.hide();
-				},
-			});
-			d.show();
-		});
+        if (listview.page.__iaes_buttons_added) return;
+        listview.page.__iaes_buttons_added = true;
 
-		listview.page.add_inner_button(__("ID After"), () => {
-			const d = new frappe.ui.Dialog({
-				title: __("Filter by ID After"),
-				fields: [
-					{
-						label: __("Document ID"),
-						fieldname: "doc_id",
-						fieldtype: "Data",
-						reqd: 1,
-					},
-				],
-				primary_action_label: __("Apply"),
-				primary_action(values) {
-					listview.filter_area.add([
-						[listview.doctype, "name", ">", values.doc_id],
-					]);
-					d.hide();
-				},
-			});
-			d.show();
-		});
-	}
+        listview.page.add_inner_button("ID Before", function () {
 
-	function patch_listview() {
-		if (!frappe.views || !frappe.views.ListView) return;
+            const d = new frappe.ui.Dialog({
+                title: "Filter ID Before",
+                fields: [
+                    {
+                        label: "Purchase Invoice ID",
+                        fieldname: "doc_id",
+                        fieldtype: "Data",
+                        reqd: 1
+                    }
+                ],
+                primary_action_label: "Apply",
+                primary_action(values) {
 
-		const proto = frappe.views.ListView.prototype;
-		if (proto._iaes_id_buttons_patched) return;
-		proto._iaes_id_buttons_patched = true;
+                    listview.filter_area.add([
+                        ["Purchase Invoice", "name", "<", values.doc_id]
+                    ]);
 
-		const original_show = proto.show;
+                    d.hide();
+                }
+            });
 
-		proto.show = function () {
-			const result = original_show.apply(this, arguments);
-			setTimeout(() => add_id_filter_buttons(this), 500);
-			return result;
-		};
-	}
+            d.show();
+        });
 
-	if (document.readyState === "loading") {
-		document.addEventListener("DOMContentLoaded", patch_listview);
-	} else {
-		patch_listview();
-	}
-})();
+
+        listview.page.add_inner_button("ID After", function () {
+
+            const d = new frappe.ui.Dialog({
+                title: "Filter ID After",
+                fields: [
+                    {
+                        label: "Purchase Invoice ID",
+                        fieldname: "doc_id",
+                        fieldtype: "Data",
+                        reqd: 1
+                    }
+                ],
+                primary_action_label: "Apply",
+                primary_action(values) {
+
+                    listview.filter_area.add([
+                        ["Purchase Invoice", "name", ">", values.doc_id]
+                    ]);
+
+                    d.hide();
+                }
+            });
+
+            d.show();
+        });
+
+    }
+};
