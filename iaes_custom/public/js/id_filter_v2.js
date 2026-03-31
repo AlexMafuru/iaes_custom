d3fa76f3762a5237631228558f957ac7c43a0aec
Alex Mafuru <alex@iaestz.com>
2026-04-01 00:56:30 +0300

    Deploy: Global ID Filters menu for all List Views

--- iaes_custom/public/js/id_filter_v2.js
+++ iaes_custom/public/js/id_filter_v2.js
@@ -1,14 +1,19 @@
 (function () {
-    const add_id_filter_group = (listview) => {
+    // The core function to inject our grouped menu
+    const inject_id_filters = (listview) => {
+        // Guard clause: ensure page exists and we haven't already added buttons
         if (!listview || !listview.page || listview.page.__id_filters_added) return;
-        listview.page.__id_filters_added = true;
-
-        // Create a single dropdown menu called "ID Filters"
-        // This will appear after the "List View" button
+        
+        // 1. Add "Before" to the "ID Filters" group
         listview.page.add_inner_button(__("Before"), () => {
-            const d = new frappe.ui.Dialog({
+            let d = new frappe.ui.Dialog({
                 title: __("Filter: ID Before"),
-                fields: [{ label: __("Document ID"), fieldname: "doc_id", fieldtype: "Data", reqd: 1 }],
+                fields: [{ 
+                    label: __("Document ID"), 
+                    fieldname: "doc_id", 
+                    fieldtype: "Data", 
+                    reqd: 1 
+                }],
                 primary_action_label: __("Apply"),
                 primary_action(values) {
                     listview.filter_area.add([[listview.doctype, "name", "<", values.doc_id]]);
@@ -16,12 +21,18 @@
                 }
             });
             d.show();
-        }, __("ID Filters")); // Grouping name
+        }, __("ID Filters"));
 
+        // 2. Add "After" to the same "ID Filters" group
         listview.page.add_inner_button(__("After"), () => {
-            const d = new frappe.ui.Dialog({
+            let d = new frappe.ui.Dialog({
                 title: __("Filter: ID After"),
-                fields: [{ label: __("Document ID"), fieldname: "doc_id", fieldtype: "Data", reqd: 1 }],
+                fields: [{ 
+                    label: __("Document ID"), 
+                    fieldname: "doc_id", 
+                    fieldtype: "Data", 
+                    reqd: 1 
+                }],
                 primary_action_label: __("Apply"),
                 primary_action(values) {
                     listview.filter_area.add([[listview.doctype, "name", ">", values.doc_id]]);
@@ -29,16 +40,21 @@
                 }
             });
             d.show();
-        }, __("ID Filters")); // Grouping name
+        }, __("ID Filters"));
+
+        // Mark as added so we don't duplicate on every refresh
+        listview.page.__id_filters_added = true;
     };
 
-    // Use a robust hook to ensure it loads on every refresh/route change
+    // GLOBAL HOOK: We patch the standard ListView header refresh
+    // This ensures that whenever ANY list is loaded or filtered, our buttons are checked
     $(document).on("app_ready", function () {
-        frappe.views.ListView.prototype.refresh_header = (function (original) {
-            return function () {
-                original.apply(this, arguments);
-                add_id_filter_group(this);
+        if (frappe.views.ListView) {
+            const original_refresh = frappe.views.ListView.prototype.refresh_header;
+            frappe.views.ListView.prototype.refresh_header = function () {
+                original_refresh.apply(this, arguments);
+                inject_id_filters(this);
             };
-        })(frappe.views.ListView.prototype.refresh_header);
+        }
     });
 })();
