03aa5a60e482dfc671841cac3451059ae7316e87
Alex Mafuru <alex@iaestz.com>
2026-04-19 01:34:27 +0300

    fix: patch set_conditions for visual dropdown v3

--- iaes_custom/public/js/filter_patch.js
+++ iaes_custom/public/js/filter_patch.js
@@ -1,29 +1,35 @@
-(function() {
-    function addComparisonOptions(selectEl) {
-        const options = Array.from(selectEl.options).map(o => o.value);
-        if (options.includes(">=")) return;
-        
-        const notEqIndex = options.indexOf("!=");
-        const extras = [">", "<", ">=", "<="];
-        
-        extras.forEach(op => {
-            const opt = new Option(op, op);
-            if (notEqIndex !== -1) {
-                selectEl.add(opt, notEqIndex + 1);
-            } else {
-                selectEl.add(opt);
-            }
-        });
-    }
+frappe.after_ajax(function () {
+    if (!frappe.ui || !frappe.ui.Filter) return;
+    if (frappe.ui.Filter.prototype._iaestz_v3) return;
+    frappe.ui.Filter.prototype._iaestz_v3 = true;
 
-    const observer = new MutationObserver(function(mutations) {
-        mutations.forEach(function(mutation) {
-            mutation.addedNodes.forEach(function(node) {
-                if (!node.querySelectorAll) return;
-                node.querySelectorAll('.filter-field select, .filter-list select').forEach(addComparisonOptions);
+    const original_set_conditions = frappe.ui.Filter.prototype.set_conditions;
+
+    frappe.ui.Filter.prototype.set_conditions = function () {
+        original_set_conditions.call(this);
+
+        const target_types = [
+            "Data", "Small Text", "Text",
+            "Long Text", "Link", "Dynamic Link"
+        ];
+
+        if (target_types.includes(this.fieldtype)) {
+            const extras = [">", "<", ">=", "<="];
+            const $select = this.field && this.field.$select;
+            if (!$select) return;
+
+            extras.forEach(op => {
+                if (!$select.find(`option[value="${op}"]`).length) {
+                    const $notEq = $select.find('option[value="!="]');
+                    const $opt = $(`<option value="${op}">${op}</option>`);
+                    if ($notEq.length) {
+                        $notEq.after($opt);
+                    } else {
+                        $select.append($opt);
+                    }
+                }
             });
-        });
-    });
-
-    observer.observe(document.body, { childList: true, subtree: true });
-})();
+            $select.trigger("change");
+        }
+    };
+});
