ef9d5a964a8430b42899cf2c85db436c978487e3
Alex Mafuru <alex@iaestz.com>
2026-04-19 09:43:46 +0300

    fix: remove >, <, >=, <= from invalid_condition_map for Link fields v4

--- iaes_custom/public/js/filter_patch.js
+++ iaes_custom/public/js/filter_patch.js
@@ -1,35 +1,22 @@
 frappe.after_ajax(function () {
     if (!frappe.ui || !frappe.ui.Filter) return;
-    if (frappe.ui.Filter.prototype._iaestz_v3) return;
-    frappe.ui.Filter.prototype._iaestz_v3 = true;
+    if (frappe.ui.Filter.prototype._iaestz_v4) return;
+    frappe.ui.Filter.prototype._iaestz_v4 = true;
 
     const original_set_conditions = frappe.ui.Filter.prototype.set_conditions;
 
     frappe.ui.Filter.prototype.set_conditions = function () {
         original_set_conditions.call(this);
 
-        const target_types = [
-            "Data", "Small Text", "Text",
-            "Long Text", "Link", "Dynamic Link"
-        ];
+        // Remove >, <, >=, <= from invalid conditions for Link and Data fields
+        const ops_to_allow = [">", "<", ">=", "<="];
+        const fields_to_patch = ["Link", "Dynamic Link", "Data", "Small Text"];
 
-        if (target_types.includes(this.fieldtype)) {
-            const extras = [">", "<", ">=", "<="];
-            const $select = this.field && this.field.$select;
-            if (!$select) return;
-
-            extras.forEach(op => {
-                if (!$select.find(`option[value="${op}"]`).length) {
-                    const $notEq = $select.find('option[value="!="]');
-                    const $opt = $(`<option value="${op}">${op}</option>`);
-                    if ($notEq.length) {
-                        $notEq.after($opt);
-                    } else {
-                        $select.append($opt);
-                    }
-                }
-            });
-            $select.trigger("change");
-        }
+        fields_to_patch.forEach(fieldtype => {
+            if (this.invalid_condition_map[fieldtype]) {
+                this.invalid_condition_map[fieldtype] = this.invalid_condition_map[fieldtype]
+                    .filter(op => !ops_to_allow.includes(op));
+            }
+        });
     };
 });
